35f89c85825f2a0e1e2ca974a8e88428366aec37
zulfi345 <zulfitjahnu5@gmail.com>
2026-04-10 16:06:40 +0700

    Update label text in main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from kivy.uix.label import Label
 
 class MyApp(App):
     def build(self):
-        return Label(text='Vecta Kasir Berhasil')
+        return Label(text='Vecta Kasir')
 
-if __name__ == '__main__':
-    MyApp().run()
+MyApp().run()
